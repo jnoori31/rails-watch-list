1cfbe67bb08e9cd2d0d25f70ffd0521f47499cf1
Jonas Noori <jonasgeneralassembly@gmail.com>
2021-04-29 20:58:07 +0100

    controller for bookmarks and corresponsding view created- with nested route

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -1,2 +1,35 @@
 class BookmarksController < ApplicationController
+
+def new
+    @bookmark = Bookmark.new
+    @list = List.find(params[:list_id])
+  end
+
+  def create
+    @bookmark = Bookmark.new(bookmark_params)
+    @list = List.find(params[:list_id])
+
+    @bookmark.list = @list
+    if @bookmark.save
+      redirect_to list_path(@list)
+    else
+      render :new
+    end
+  end
+
+  def show
+    @bookmark = Bookmark.find(params[:id])
+  end
+
+
+  def destroy
+    @bookmark.destroy
+    redirect_to bookmarks_path
+  end
+
+  private
+
+  def bookmark_params
+    params.require(:bookmark).permit(:comment, :movie_id)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@
 
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :lists, only: %i[index show new create]
+  resources :lists, only: %i[index show new create] do
+    resources :bookmarks, only: %i[create new destroy]
+  end
 end
